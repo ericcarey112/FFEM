8b56f668bfe576a75790992065b38b89a6ebc31f
ericcarey112 <ericcarey112@gmail.com>
2024-04-04 13:56:44 -0400

    deleted unnecessary files

--- repos/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ repos/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b985595aea688086377009a84db17b41c4fbfeab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06cad5fc14dfdf3a811a6e3ae02a59f0b1579e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- repos/FEMAssembly/obj/Debug/net8.0/FEMAssembly.AssemblyInfo.cs
+++ repos/FEMAssembly/obj/Debug/net8.0/FEMAssembly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FEMAssembly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b985595aea688086377009a84db17b41c4fbfeab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06cad5fc14dfdf3a811a6e3ae02a59f0b1579e3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FEMAssembly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FEMAssembly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
